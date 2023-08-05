98d728a23b330ecfdf78b1b8b8625b4cb582a51e
6a6ak <babak.ahari@gmail.com>
2023-08-06 01:31:20 +0200

    sofar so good

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 function generateBoxes() {
     const input = document.getElementById('inputText').value.trim();
-    const lines = input.split("\n");
+    const lines = input.split("\n").filter(line => line.trim() !== "");
 
     const outputContainer = document.getElementById('outputContainer');
-    outputContainer.innerHTML = ''; // clear previous boxes
+    outputContainer.innerHTML = '';
 
     lines.forEach((line, index) => {
         const [num, text] = line.split('-');
@@ -13,9 +13,26 @@ function generateBoxes() {
         lineDiv.className = 'line';
 
         texts.forEach((t) => {
+            // Create checkbox
+            const checkbox = document.createElement('input');
+            checkbox.type = 'checkbox';
+            checkbox.addEventListener('change', function() {
+                if (this.checked) {
+                    boxContent.classList.add('text-strike');
+                } else {
+                    boxContent.classList.remove('text-strike');
+                }
+            });
+
             const box = document.createElement('div');
             box.className = 'box';
-            box.textContent = `${num}- ${t}`;
+
+            const boxContent = document.createElement('span');
+            boxContent.textContent = `${num}- ${t}`;
+
+            // Append elements
+            box.appendChild(checkbox);
+            box.appendChild(boxContent);
             lineDiv.appendChild(box);
         });
 
@@ -28,4 +45,14 @@ function generateBoxes() {
             outputContainer.appendChild(arrow);
         }
     });
+       // After generating boxes, hide input section and show chart section
+       document.getElementById('inputSection').style.display = 'none';
+       document.getElementById('chartSection').style.display = 'block';
+}
+
+
+function showInput() {
+    // Show the input section and hide the chart section
+    document.getElementById('inputSection').style.display = 'block';
+    document.getElementById('chartSection').style.display = 'none';
 }
